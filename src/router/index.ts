f63cca37da52606b4700ab681015ee815333752b
Marcos Rivera <mjrivera@knowmadmood.com>
2026-05-08 11:26:45 +0200

    feat: actualización de la ruta principal como path: '/'

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/shop',
+      path: '/',
       name: 'shop',
       component: () => import('../modules/shop/layouts/ShopLayout.vue'),
       children: [
