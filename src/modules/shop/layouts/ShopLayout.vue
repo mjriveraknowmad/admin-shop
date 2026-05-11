963ad8190eb69dbe0e7afa905f22082545bd7009
Marcos Rivera <mjrivera@knowmadmood.com>
2026-05-11 13:55:51 +0200

    refactor: refactorización mínima del componente ShopLayout

--- src/modules/shop/layouts/ShopLayout.vue
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -1,19 +1,10 @@
 <template>
 
-  <!-- Header Navbar -->
-  <TopMenu />
+  <top-menu />
 
-  <!-- Title -->
-  <div class="pt-32  bg-white">
-    <h1 class="text-center text-2xl font-bold text-gray-800">All Products</h1>
-  </div>
+  <router-view />
 
-  <!-- RouterView -->
-  <RouterView />
-
-  <!-- Footer -->
-  <CustomFooter />
-
+  <custom-footer />
 </template>
 
 
